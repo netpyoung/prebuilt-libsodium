7172a6559dd1df01666629ca5ec24b2414d6a00e
netpyoung <netpyoung@gmail.com>
2023-10-15 04:20:34 +0900

    change: libsodium 1.0.19

--- build-on-windows.bat
+++ build-on-windows.bat
@@ -1,4 +1,4 @@
 
-git clone -b 1.0.18 --depth 1 https://github.com/jedisct1/libsodium.git
+git clone -b 1.0.19 --depth 1 https://github.com/jedisct1/libsodium.git
 
 libsodium\builds\msvc\build\buildbase.bat libsodium\builds\msvc\vs2019\libsodium.sln 16
