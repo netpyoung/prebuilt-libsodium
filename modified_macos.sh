e013116e30c2a931d5753923d2ebc2ca3baf1b5f
netpyoung <netpyoung@gmail.com>
2023-10-15 03:46:24 +0900

    rename: modified_osx.sh => modified_macos.sh & Add ARCHFLAGS

--- modified_macos.sh
+++ modified_macos.sh
@@ -1,6 +1,7 @@
 #! /bin/sh
 
-# modified osx.sh for support both arm64 and x86_64
+# modified macos.sh for support both arm64 and x86_64
+export ARCHFLAGS="-arch x86_64 -arch arm64"
 
 export PREFIX="$(pwd)/libsodium-osx"
 export MACOS_VERSION_MIN=${MACOS_VERSION_MIN-"10.10"}
@@ -11,21 +12,18 @@ else
   export LIBSODIUM_ENABLE_MINIMAL_FLAG=""
 fi
 
-
 NPROCESSORS=$(getconf NPROCESSORS_ONLN 2>/dev/null || getconf _NPROCESSORS_ONLN 2>/dev/null)
 PROCESSORS=${NPROCESSORS:-3}
 
 mkdir -p $PREFIX || exit 1
 
-export CFLAGS="-arch x86_64 -arch arm64 -mmacosx-version-min=${MACOS_VERSION_MIN} -O2 -g"
-export LDFLAGS="-arch x86_64 -arch arm64 -mmacosx-version-min=${MACOS_VERSION_MIN}"
+export CFLAGS="${ARCHFLAGS} -mmacosx-version-min=${MACOS_VERSION_MIN} -Ofast"
+export LDFLAGS="${ARCHFLAGS} -mmacosx-version-min=${MACOS_VERSION_MIN}"
 
-make distclean > /dev/null
+make distclean >/dev/null
 ./configure ${LIBSODIUM_ENABLE_MINIMAL_FLAG} \
-            --prefix="$PREFIX" || exit 1
-
-
+  --prefix="$PREFIX" || exit 1
 make -j${PROCESSORS} check && make -j${PROCESSORS} install || exit 1
 
 # Cleanup
-make distclean > /dev/null
+make distclean >/dev/null
