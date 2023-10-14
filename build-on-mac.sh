312a2854eebc059bc2a564da474a4271e9b32b0b
netpyoung <netpyoung@gmail.com>
2023-10-15 03:46:52 +0900

    add: build-on-mac.sh

---
+++ build-on-mac.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+set -e
+export LIBSODIUM_FULL_BUILD=true
+
+readonly IOS_MIN_VERSION=6.0
+
+# ===========================
+# macOs, iOs
+# ===========================
+# [brew] install dependencies
+# packages='libtool autoconf automake'
+# brew update
+# for pkg in ${packages}; do
+#     if brew list -1 | grep -q "^${pkg}\$"; then
+#         echo "Package '$pkg' is installed"
+#         #brew upgrade $pkg
+#     else
+#         echo "Package '$pkg' is not installed"
+#         brew install $pkg
+#     fi
+# done
+# brew upgrade ${packages} || true
+
+
+# [variable]
+ROOT=$(pwd)
+DIR_DEST=${ROOT}/output
+DIR_LIBSODIUM=${ROOT}/libsodium
+
+
+# [src] libsodium
+git clone -b 1.0.19 --depth 1 https://github.com/jedisct1/libsodium.git $DIR_LIBSODIUM && cd $DIR_LIBSODIUM
+
+# replace macos.sh with modified_macos.sh
+cp $ROOT/modified_macos.sh $DIR_LIBSODIUM/dist-build/macos.sh
+./autogen.sh
+./dist-build/macos.sh
+mkdir -p $DIR_DEST/Plugins/x64
+cp $DIR_LIBSODIUM/libsodium-osx/lib/libsodium.*.dylib $DIR_DEST/Plugins/x64/sodium.bundle
+
+echo "lipo -info $DIR_LIBSODIUM/libsodium-osx/lib/libsodium.*.dylib"
+lipo -info $DIR_LIBSODIUM/libsodium-osx/lib/libsodium.*.dylib
