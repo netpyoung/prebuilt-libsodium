8c04b2a83a26b7c1547bd7712a3cc33dbf7ac00a
netpyoung <netpyoung@gmail.com>
2025-10-20 11:00:49 +0900

    change: build-on-windows.bat => build-on-windows.ps1

--- build-on-windows.bat
+++
@@ -1,4 +0,0 @@
-
-git clone --branch 1.0.20-RELEASE --depth 1 https://github.com/jedisct1/libsodium.git
-
-libsodium\builds\msvc\build\buildbase.bat libsodium\builds\msvc\vs2022\libsodium.sln 17
---
+++ build-on-windows.ps1
@@ -0,0 +1,33 @@
+# [variable]
+$VERSION = "1.0.20-RELEASE"
+
+git clone --branch $VERSION --depth 1 https://github.com/jedisct1/libsodium.git
+
+$solution = "libsodium\builds\msvc\vs2022\libsodium.sln"
+
+New-Item -ItemType Directory -Path "libsodium\bin" -Force | Out-Null
+$log = "libsodium\bin\build.log"
+
+Write-Host "Platform=x86"
+Write-Host "Configuration=DynRelease"
+& msbuild /m /v:n /p:Configuration=DynRelease /p:Platform=Win32 $solution | Tee-Object -FilePath $log -Append
+if ($LASTEXITCODE -ne 0)
+{
+    Write-Host "*** ERROR, build terminated early, see: $log"
+    Get-Content $log
+    exit 1
+}
+
+Write-Host "Platform=x64"
+Write-Host "Configuration=DynRelease"
+& msbuild /m /v:n /p:Configuration=DynRelease /p:Platform=x64 $solution | Tee-Object -FilePath $log -Append
+if ($LASTEXITCODE -ne 0)
+{
+    Write-Host "*** ERROR, build terminated early, see: $log"
+    Get-Content $log
+    exit 1
+}
+
+# ARM64
+
+Write-Host "Complete: $solution"
